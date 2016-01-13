8567df125dad633547368c8f648c101ed77df478
ysw070 <ysw070@naver.com>
2016-01-13 19:15:44 +0900

    로그인 기능 댔어 근데 헤더 는 아직..

--- allstar/src/main/java/allstar/pms/controller/ajax/AuthController.java
+++ allstar/src/main/java/allstar/pms/controller/ajax/AuthController.java
@@ -1,7 +1,5 @@
 package allstar.pms.controller.ajax;
 
-import java.util.HashMap;
-
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
@@ -10,14 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import allstar.pms.dao.MemberDao;
 import allstar.pms.domain.AjaxResult;
 import allstar.pms.domain.Member;
+import allstar.pms.service.MemberService;
 
 @Controller("ajax.AuthController")
 @RequestMapping("/auth/ajax/*")
 public class AuthController { 
-  @Autowired MemberDao memberDao;
+  @Autowired MemberService memberService;
   
   @RequestMapping(value="login", method=RequestMethod.GET)
   public String loginform() {
@@ -31,18 +29,14 @@ public class AuthController {
       HttpServletResponse response, 
       HttpSession session) {
 
-    HashMap<String,Object> paramMap = new HashMap<>();
-    paramMap.put("id", id);
-    paramMap.put("password", password);
+
     
-    Member member = memberDao.login(paramMap);
+    Member member = memberService.retrieve(id, password);
 
     if (member == null) { // 로그인 실패!
-      session.invalidate(); // 세션을 무효화시킴. => 새로 세션 객체 생성!
       return new AjaxResult("failure", null);
     }
 
-    session.setAttribute("loginUser", member);
     return new AjaxResult("success", member);
   }
   
