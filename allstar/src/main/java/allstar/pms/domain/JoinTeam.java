1c8cc6265b2d5e609d0829ccb0622a274a3b1005
ysw070 <ysw070@naver.com>
2016-01-27 12:11:11 +0900

    회원정보 팀 쪼인  했지만 1번 팀에 문제가 생김 . 문제 : 쪼인 시켜놔서 팀 삭제가 안됨

---
+++ allstar/src/main/java/allstar/pms/domain/JoinTeam.java
@@ -0,0 +1,74 @@
+package allstar.pms.domain;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class JoinTeam implements Serializable {
+  private static final long serialVersionUID = 1L;
+  
+  protected int     mno;
+  protected int     tno;
+  protected Date  createdDate;
+  protected String content;
+  protected int     state;
+  protected int     level;
+  protected Team team;
+  protected Member member;
+  
+  
+ 
+  public Team getTeam() {
+    return team;
+  }
+  public void setTeam(Team team) {
+    this.team = team;
+  }
+  public Member getMember() {
+    return member;
+  }
+  public void setMember(Member member) {
+    this.member = member;
+  }
+  public int getMno() {
+    return mno;
+  }
+  public void setMno(int mno) {
+    this.mno = mno;
+  }
+  public int getTno() {
+    return tno;
+  }
+  public void setTno(int tno) {
+    this.tno = tno;
+  }
+  public Date getCreatedDate() {
+    return createdDate;
+  }
+  public void setCreatedDate(Date createdDate) {
+    this.createdDate = createdDate;
+  }
+  public String getContent() {
+    return content;
+  }
+  public void setContent(String content) {
+    this.content = content;
+  }
+  public int getState() {
+    return state;
+  }
+  public void setState(int state) {
+    this.state = state;
+  }
+  public int getLevel() {
+    return level;
+  }
+  public void setLevel(int level) {
+    this.level = level;
+  }
+  @Override
+  public String toString() {
+    return "JoinTeam [mno=" + mno + ", tno=" + tno + ", createdDate=" + createdDate + ", content=" + content
+        + ", state=" + state + ", level=" + level + ", team=" + team + ", member=" + member + "]";
+  }
+
+}
